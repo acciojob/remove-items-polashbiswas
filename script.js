e3b16c015e422629b16e912213b9d3389c48a3c6
polashbiswas <30291056+polashbiswas@users.noreply.github.com>
2023-04-23 01:41:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+var colorselect = document.getElementById("colorSelect");
+// console.log(colorselect[1].innerHTML);
+let btn = document.querySelector("input[type='button']");
+btn.addEventListener("click", function(){
+	let selectItem = colorselect.options[colorselect.selectedIndex];
+	colorselect.removeChild(selectItem);
+});
